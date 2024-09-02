dab82e60e62cb7e555fbdd4b3ed52e44c372a6d5
SANJAY <sanjaypk986@gmail.com>
2024-09-02 22:06:10 +0530

    Rename and update files

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { RouterProvider } from 'react-router-dom'
 import{ Toaster } from 'react-hot-toast';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { router } from './routes/Root';
 
 
 const App = () => {
