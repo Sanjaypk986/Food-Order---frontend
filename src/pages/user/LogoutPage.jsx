e8fbe3f7a623d85ec81f5f814371be534130c56c
SANJAY <sanjaypk986@gmail.com>
2024-09-03 16:37:03 +0530

    logout page debugging

--- src/pages/user/LogoutPage.jsx
+++ src/pages/user/LogoutPage.jsx
@@ -13,10 +13,17 @@ const LogoutPage = () => {
     const logOut = async () => {
       try {
         const response = await userLogout();
-        console.log(response);
+        console.log("Logout API response:", response);
 
         if (response.success) {
+          // Debug: Log current cookies
+          console.log("Cookies before clearing:", document.cookie);
+          
           document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+          
+          // Debug: Log cookies after clearing
+          console.log("Cookies after clearing:", document.cookie);
+          
           dispatch(resetUser()); // Clear the user state in Redux
           toast.success("Logout successful");
           navigate("/");
