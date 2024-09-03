33513a3f00265e2acc04efc1b0e44627fc0c993d
SANJAY <sanjaypk986@gmail.com>
2024-09-03 16:04:48 +0530

    logout token fixing

--- src/pages/user/LogoutPage.jsx
+++ src/pages/user/LogoutPage.jsx
@@ -7,22 +7,30 @@ import { resetUser } from "../../features/user/userSlice";
 
 const LogoutPage = () => {
   const navigate = useNavigate();
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  
   useEffect(() => {
     const logOut = async () => {
       try {
         const response = await userLogout();
+        console.log(response);
+
         if (response.success) {
+          document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
           dispatch(resetUser()); // Clear the user state in Redux
           toast.success("Logout successful");
           navigate("/");
+        } else {
+          toast.error("Logout failed");
         }
       } catch (error) {
-        console.log(error);
+        console.error("Logout error:", error);
+        toast.error("An error occurred during logout");
       }
     };
+
     logOut();
-  }, []);
+  }, [navigate, dispatch]);
 
   return (
     <main>
