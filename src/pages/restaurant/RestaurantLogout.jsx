4f87589b13ffce1ffe55817e667163aee804f2d4
SANJAY <sanjaypk986@gmail.com>
2024-10-07 21:59:27 +0530

    changed styles of logout pages

--- src/pages/restaurant/RestaurantLogout.jsx
+++ src/pages/restaurant/RestaurantLogout.jsx
@@ -26,11 +26,34 @@ const RestaurantLogoutPage = () => {
     }, [navigate]);
   
     return (
-      <main className="container mx-auto px-2 min-h-screen">
-        <section>
-          <h1>Logging out....</h1>
-        </section>
-      </main>
+      <main className="flex items-center justify-center min-h-screen ">
+      <section className="bg-white rounded-lg shadow-md p-8 text-center">
+        <h1 className="text-2xl font-bold mb-4">Logging Out...</h1>
+        <p className="text-gray-600">Please wait while we log you out.</p>
+        <div className="flex justify-center mt-4">
+          <svg
+            className="animate-spin h-8 w-8 text-gray-500"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="4"
+            />
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8v4l4-4-4-4v4a8 8 0 10-8 8z"
+            />
+          </svg>
+        </div>
+      </section>
+    </main>
     );
   };  
 
