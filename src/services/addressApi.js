3da8e8814da911ca1fd59b8c444f6b4e39cf67f0
SANJAY <sanjaypk986@gmail.com>
2024-10-13 21:51:53 +0530

    changed some alignments of codes

--- src/services/addressApi.js
+++ src/services/addressApi.js
@@ -1,29 +1,23 @@
 import { axiosInstance } from "../config/axiosInstance";
 
-
 // Create Address
 export const createAddress = async (data) => {
-    try {
-      const response = await axiosInstance.post(`/address/create`, 
-        data
-      );
-      return response?.data; 
-    } catch (error) {
-      console.log("Error in creating Address:", error.message);
-      return null; 
-    }
-  };
-
+  try {
+    const response = await axiosInstance.post(`/address/create`, data);
+    return response?.data;
+  } catch (error) {
+    console.log("Error in creating Address:", error.message);
+    return null;
+  }
+};
 
 // Update Address
 export const updateAddress = async (data) => {
   try {
-    const response = await axiosInstance.patch(`/address/update`, 
-      data
-    );
-    return response?.data; 
+    const response = await axiosInstance.patch(`/address/update`, data);
+    return response?.data;
   } catch (error) {
     console.log("Error in updating Address:", error.message);
-    return null; 
+    return null;
   }
 };
